97c4c16d5740d259230c9e771b1f9dc4758f9c6e
Shaghik66 <kaprielianshaghig961@gmail.com>
2026-04-30 23:45:51 +0400

    ToDo Input Creation With Material UI

--- src/components/atoms/AddToDoInput/AddToDoInput.tsx
+++ src/components/atoms/AddToDoInput/AddToDoInput.tsx
@@ -1,12 +1,29 @@
 import { type ChangeEvent } from "react";
+import {Stack, TextField} from "@mui/material";
 
 type AddTodoPropsType = {
-  text : string
-  changeText: (e : ChangeEvent<HTMLInputElement>) => void;
+  text: string;
+  changeText: (e: ChangeEvent<HTMLInputElement>) => void;
 };
 
-export const AddToDoInput = ({text, changeText} : AddTodoPropsType) => {
+
+export function AddToDoInput({ text, changeText }: AddTodoPropsType) {
   return (
-    <input value={text} onChange={changeText} type="text"  />
-  )
+    <Stack
+      component="form"
+      sx={{ width: "25ch" }}
+      spacing={2}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField
+        hiddenLabel
+        id="filled-hidden-label-small"
+        variant="filled"
+        size="small"
+        value={text}
+        onChange={changeText}
+      />
+    </Stack>
+  );
 }
