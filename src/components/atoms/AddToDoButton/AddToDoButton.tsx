fdbcd142dd313b960a48045168f2942ba1e82e6c
Shaghik66 <kaprielianshaghig961@gmail.com>
2026-04-30 23:39:32 +0400

    Send Button Creation With Material UI

--- src/components/atoms/AddToDoButton/AddToDoButton.tsx
+++ src/components/atoms/AddToDoButton/AddToDoButton.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import {Box, Button} from "@mui/material";
 
 type InputButtonPropsType = {
   addTodo: () => void;
 };
 
-export const AddToDoButton = ({ addTodo } : InputButtonPropsType) => {
-  return <button onClick={addTodo}>AddToDoButton</button>;
-};
+export function AddToDoButton({ addTodo }: InputButtonPropsType) {
+  return (
+    <Box sx={{ "& button": { m: 1 } } }>
+      <div>
+        <Button onClick={addTodo} variant="contained" size="small">
+            Send
+        </Button>
+      </div>
+    </Box>
+  );
+}
